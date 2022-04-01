1140d5205fbb336be5ef9efaedd8eb025eeb6929
Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>
2022-04-01 19:29:58 +0530

    Addition of CLI utils
    Signed-off-by: Abhijith Ganesh <67182544+AbhijithGanesh@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,42 @@
-use clap::Parser;
+use clap::{Arg, ArgMatches, Command};
+mod cli {
+    pub mod initializer;
+}
 
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[clap(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[clap(short, long, default_value_t = 1)]
-    count: u8,
+pub fn configurator() -> [Command<'static>; 1] {
+    return [Command::new("config")
+        .short_flag('c')
+        .about("Helps you configure Express-Cassandra")
+        .args(&[Arg::new("construct_new_connector")
+            .short('n')
+            .help("Construct a new connection with the new tag. Cassey -c -n new")
+            .takes_value(true)])];
 }
 
 fn main() {
-    let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+    let cli:ArgMatches = Command::new("Cassey CLI")
+        .version("0.1.0")
+        .about("This program lets you configure, work with Cassandra and Express-Cassandra without hassle.\nThis will configure your cassandra cluster for your express cassandra project.")
+        .after_help(
+            "Longer explanation to appear after the options when \
+                 displaying the help information from --help or -h",
+        )
+        .subcommands(configurator())
+        .get_matches();
+    println!(
+        "{:#?}",
+        cli.subcommand_matches("config")
+            .unwrap()
+            .value_of("construct_new_connector")
+            .unwrap()
+    );
+    if cli
+        .subcommand_matches("config")
+        .unwrap()
+        .value_of("construct_new_connector")
+        .unwrap()
+        == "new"
+    {
+        cli::initializer::create();
     }
 }
