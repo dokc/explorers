2eba5a65c3c3d63e0e2743ae0b116eaaa77f132e
Abhijith Ganesh <githubwatchabhijithganesh@gmail.com>
2022-10-09 19:39:52 +0530

    Update guide-to-open-source.jsx

--- src/pages/guide-to-open-source.jsx
+++ src/pages/guide-to-open-source.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { RiOpenSourceFill } from "react-icons/ri";
 import image from "./../images/dokc-logo-white.svg";
 
 let Guide = () => {
@@ -29,11 +28,73 @@ let Guide = () => {
       </section>
       <section className="pt-8 grid lg:grid-cols-12">
         <section className="col-start-3 col-span-8">
-          <section className="text-white font-bold text-4xl items-center">
+          <section className="text-white font-bold text-4xl items-center font-mono">
             What is Open Source ?
           </section>
-          <section className="pt-4 text-white font-regular text-xl">
+          <section className="pt-6 text-white font-regular text-2xl font-mono">
             #How to get started? #What does it mean ? #Why should you ?
+          </section>
+          <section className="py-2 text-white font-regular text-lg">
+            Generally, Open Source software is software that can be freely
+            accessed, used, changed, and shared (in unabridged form) by anyone.
+            Open source software is made by many people, and distributed under
+            licenses that comply with the Open Source Definition. Some famous
+            open source licenses are MIT, BSD-II, BSD-III.
+          </section>
+          <section className="py-2 text-3xl text-white font-mono font-semibold">
+            How should you get started ?
+          </section>
+          <section className="py-2 text-white font-regular text-lg">
+            To get started with open source, go to your favourite repository,
+            For example{" "}
+            <a
+              href="https://github.com/dokc/dokc.github.io"
+              className="underline"
+            >
+              this github page .
+            </a>
+            To contribute to this project, you will have to fork the repository,
+            you can fork it by clicking on the fork button. Once forked you
+            should be able to see the repository in your account. The repository
+            will be available to you and you can maintain the fork if you choose
+            to build a new stream of features. If you want to implement a
+            feature on the parent repository, clone it.
+            <section className="p-2 text-white font-light font-mono text-md flex justify-space-around px-8">
+              git clone [your repository url]
+            </section>
+            Once cloned, you can go on to repository and implement the necessary
+            changes.
+            <b className="px-2">
+              {" "}
+              Before you commit the changes, ensure you've read the contributing
+              guide to avoid potential rejections.
+            </b>
+            Once completed, you can jump into the pull request section of this
+            blog.
+          </section>
+          <section className="text-3xl text-white font-mono font-semibold">
+            How to make a pull request / merge request ?
+          </section>
+          <section className="py-2 text-white font-regular text-lg">
+            <b>Congratulations</b>, you've made your first changes, locally. Now
+            these changes need to be pushed into your remote repository hosted
+            on <i className="emphasis"> GitHub / GitLab</i>. To do so you can
+            run the following commands.
+            <section className="p-2 text-white font-light font-mono text-md flex justify-space-around px-8">
+              git commit -s -m [Your Message]
+            </section>
+            Once you've commited, you can push it to your remote using this
+            command.
+            <section className="p-2 text-white font-light font-mono text-md flex justify-space-around px-8">
+              git push -u [remote-name] [branch-name]
+            </section>
+            You've pushed the changes to repository, now you will be prompted by
+            GitHub to create a pull request.
+            <br/>
+            <em className="py-6 text-3xl font-mono">
+              VOILA ! You have made your first ever contribution to an
+              open-source project
+            </em>
           </section>
         </section>
       </section>
